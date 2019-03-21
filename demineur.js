fac69b61ce8ca958a96db0faeb2458e05ddde0b1
DoubleZ <zbludo@hotmail.com>
2019-03-21 14:16:26 +0100

    Implémentation des classes Case et Board

--- demineur.js
+++ demineur.js
@@ -6,36 +6,61 @@ var difficultes = {
 	"master" : [30, 30, 250]
 }
 var niveauChoisi = "master";
+var table;
 
 class Board{
-	constructor(x,y){
+	constructor(x,y, nbrMines){
+		this.nbrMines = nbrMines;
 		this.table = [];
-		for(i=0; i<x; i++){
+
+		document.getElementById("gameboard").innerHTML = "";
+		var table = document.createElement("table");
+		document.getElementById("gameboard").appendChild(table);
+
+		for(var i=0; i<x; i++){
+			var tr = document.createElement("tr");
+			document.querySelector("table").appendChild(tr);
 			this.table.push([]);
-			for(j=0; j<y; j++){
-				this.table[i].push(new Case());
+			for(var j=0; j<y; j++){
+				var td = document.createElement("td");
+				var img = document.createElement("img");
+				img.src = "images/empty.png";
+				td.appendChild(img);
+				tr.appendChild(td);
+				this.table[i].push(new Case(td));
 			}
 		}
 	}
 }
 
 class Case{
-	constructor(){
+	constructor(elm){
 		this.mine = false;
 		this.discovered = false;
+		this.element = elm;
 	}
 }
 
 $("#newgame").on("click", function(){
-	niveauChoisi = $("#level").children("option:selected").val();
+	var level = $("#level").children("option:selected").val();
+	var sizeX = difficultes[level][0];
+	var sizeY = difficultes[level][1];
+	var nbrMines = difficultes[level][2];
+	table = new Board(sizeX, sizeY, nbrMines);
 
-	$("#gameboard").empty().append("<table></table>");
-
-	for(var i=0 ; i<difficultes[niveauChoisi][0] ; i++){
-		$("table").append("<tr></tr>");
-	}
-
-	for(var j=0; j<difficultes[niveauChoisi][0] ; j++){
-		$("table tr").append("<td><img src='images/empty.png'></td>");
-	}
+	table.table[1][1].element.childNodes[0].src = "";
 })
+
+function randomMines(number){
+	rX = Math.floor(Math.random()*table.table[0].length);
+	rY = Math.floor(Math.random()*table.table.length);
+
+	for(i=0; i<number; i++){
+		if(!table.table[rY][rX].mine){
+			table.table[rY][rX].mine = true
+		}
+		else{
+			i--;
+		}
+	}
+}
